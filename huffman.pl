65e0b269f03480aadfdad0e57000e0195099a2d4
Stephan Wypler <s.wypler@online.de>
2013-03-26 11:12:04 +0100

    complexmerge

--- huffman.pl
+++ huffman.pl
@@ -1,2 +1,6 @@
-# count occurrences of characters
-# 
+% count occurrences of characters 
+
+% true if E is melber of list of type [[A, 2], [B, 1], [C, 1]]
+complexmember(E, [[A,B]|R]):- 
+    E = A;
+    complexmember(E, R).
