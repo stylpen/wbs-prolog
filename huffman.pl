ed907756a699f2dcc7dd9f6215da0b7b14571320
Martin Meinke <meinke.martin@googlemail.com>
2013-03-26 11:13:07 +0100

    occurences

--- huffman.pl
+++ huffman.pl
@@ -1,2 +1,11 @@
-# count occurrences of characters
-# 
+% count occurrences of characters
+% 
+sortedoccurencelist([FI|RI], O, O2]):-
+  O = [[C, F]|R],
+  (
+    member(FI, O), F:-occurences(C, O2).
+  )
+
+occurences([],X,0).
+occurences([X|T],[X],Y):- count(T,[X],Z), Y is 1+Z.
+occurences([X1|T],[X],Z):- X1\=X,count(T,[X],Z).
