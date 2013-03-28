ea891cf548dcd0266d987c16d041aee07ec5a055
Stephan Wypler <s.wypler@online.de>
2013-03-28 21:11:37 +0100

    occurencelist working

--- huffman.pl
+++ huffman.pl
@@ -6,6 +6,10 @@
 %    member(FI, O), F:-occurences(C, O2).
 %  )
 
+% OCCURENCES
+% stores number of occurences of second parameter in first parameter in third parameter.
+% occurences("test", "t", X).
+% X = 2 
 occurences([], X, 0).
 
 occurences([X|T], [X], Y):-
@@ -14,8 +18,13 @@ occurences([X|T], [X], Y):-
 occurences([X1|T], [X], Z):-
     X1 \= X, occurences(T, [X], Z).
 
+% OCCURENCELIST
+% makes list of number of occurences per character in string
+% occurencelist("test", [], X).
+% X = [[116, 2], [101, 1], [115, 1]] 
 occurencelist([], [], []).
-occurencelist([], OL, OL).
+
+occurencelist([], L, L).
 
 occurencelist([X|T], OL, NL):-
     complexmember(X, OL),
@@ -23,11 +32,11 @@ occurencelist([X|T], OL, NL):-
 
 occurencelist([X|T], OL, NL):-
     occurences([X|T], [X], E),
-    append(OL, [[X, E]], NL),
-    occurencelist(T, NL, NL).
+    append(OL, [[X, E]], HL),
+    occurencelist(T, HL, NL).
     
 
-% true if E is melber of list of type [[A, 2], [B, 1], [C, 1]]
+% true if E is member of list of type [[A, 2], [B, 1], [C, 1]]
 complexmember(E, [[A,B]|R]):- 
     E = A;
     complexmember(E, R).
