4b371198c630e466367ffd227ca1e3114f71b277
Martin Meinke <meinke.martin@googlemail.com>
2013-03-28 16:37:53 +0100

    swaplist

--- huffman.pl
+++ huffman.pl
@@ -1,10 +1,10 @@
 % count occurrences of characters
 % 
-sortedoccurencelist([FI|RI], O, O2]):-
-  O = [[C, F]|R],
-  (
-    member(FI, O), F:-occurences(C, O2).
-  )
+%sortedoccurencelist([FI|RI], O, O2]):-
+%  O = [[C, F]|R],
+%  (
+%    member(FI, O), F:-occurences(C, O2).
+%  )
 
 occurences([],X,0).
 occurences([X|T],[X],Y):- count(T,[X],Z), Y is 1+Z.
@@ -14,3 +14,8 @@ occurences([X1|T],[X],Z):- X1\=X,count(T,[X],Z).
 complexmember(E, [[A,B]|R]):- 
     E = A;
     complexmember(E, R).
+
+%does this: [[f,3], [x, 4], [r, 2]] => [[r, 2], [f,3], [x, 4]]
+swapelements([], []).
+swapelements([[F,S]], [[S,F]]).
+swapelements([[F,S]|R], [[S,F]|R2]) :- swapelements(R, R2).
