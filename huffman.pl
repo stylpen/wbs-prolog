f02060377c1762faaa352b8ff9e337d19290ee0d
Stephan Wypler <s.wypler@online.de>
2013-03-30 22:48:17 +0100

    dfs checkpoint 0

--- huffman.pl
+++ huffman.pl
@@ -1,6 +1,5 @@
-huffmanize(X) :- 
-    I = "hallo",
-    occurrencelist(I, [], OL),
+huffmanize(T, X) :- 
+    occurrencelist(T, [], OL),
     sortswapped(OL, SOL),
     occurrencelist2tree(SOL, X).
 
@@ -20,8 +19,6 @@ occurences([X1|T], [X], Z):-
 % makes list of number of occurences per character in string. first parameter is the string, second is the data structure passed to the function as input, last is the output
 % occurencelist("test", [], X).
 % X = [[116, 2], [101, 1], [115, 1]] 
-occurrencelist([], [], []).
-
 occurrencelist([], L, L).
 
 occurrencelist([X|T], OL, NL):-
@@ -42,20 +39,91 @@ complexmember(E, [[A,B]|R]):-
 % SORTSWAPPED
 % swaps, sorts, swaps back
 sortswapped([], []).
-sortswapped(I, O) :- swapelements(I, T), sort(T, S), swapelements(S, O).
+
+sortswapped(I, O):-
+    swapelements(I, T),
+    sort(T, S),
+    swapelements(S, O).
 
 % SWAPELEMENTS
 % does this: [[f,3], [x, 4], [r, 2]] => [[r, 2], [f,3], [x, 4]]
 swapelements([], []).
+
 swapelements([[F,S]], [[S,F]]).
-swapelements([[F,S]|R], [[S,F]|R2]) :- swapelements(R, R2).
+
+swapelements([[F,S]|R], [[S,F]|R2]):-
+    swapelements(R, R2).
 
 % TREE
 % takes the leftmost two items and combines them into one node, afterwards
 % sorts the list
-tree([[X,NX],[Y,NY]|R], R2) :- N is NX+NY, sortswapped([[[[X,NX],[Y,NY]],N]|R],R2).
+tree([[X,NX],[Y,NY]|R], R2):-
+    N is NX + NY, 
+    sortswapped([[[[X, NX], [Y,NY]], N]|R], R2).
 
 % FREQLIST2TREE
 % takes an occurrencelist and turns it into a tree
 occurrencelist2tree([FL], [FL]).
-occurrencelist2tree([FLA,FLB|FLR], T) :- tree([FLA,FLB|FLR], N), occurrencelist2tree(N, T).
+occurrencelist2tree([FLA,FLB|FLR], T):-
+    tree([FLA, FLB|FLR], N),
+    occurrencelist2tree(N, T).
+
+
+% Depth First Search
+dfs([T, X], T, L, L).
+
+dfs(T, G, OL, NL):-
+    [[X|R]] = T,
+    append(OL, [5], Z),
+    dfs(X, G, Z, NL),
+    write(T),
+    write('\neingepackt\n'),
+    write(OL),
+    write('\n'),
+    write(X),
+    write('\n'),
+    write('\n').
+
+dfs(T, G, OL, NL):-
+    [X, Y|R] = T,
+    append(OL, [1], Z),
+    dfs(Y, G, Z, NL),
+    write(T),
+    write('\nrechts\n'),
+    write(OL),
+    write('\n'),
+    write(Y),
+    write('\n'),
+    write('\n').
+
+dfs(T, G, OL, NL):-
+    [[X], M] = T,    
+    append(OL, [4], Z),
+    dfs(X, G, Z, NL),
+    write(T),
+    write('\nanders eingepackt\n'),
+    write(OL),
+    write('\n'),
+    write(X),
+    write('\n'),
+    write('\n').
+
+dfs(T, G, OL, NL):-
+    [X|R] = T,
+    append(OL, [0], Z),
+    dfs(X, G, Z, NL),
+    write(T),
+    write('\nlinks\n'),
+    write(OL),
+    write('\n'),
+    write(X),
+    write('\n'),
+    write('\n').
+
+%[[[[121, 1], [[[101, 1], [104, 1]], 2]], 3]]
+%  [[121, 1], [[[101, 1], [104, 1]], 2]]
+%             [[[101, 1], [104, 1]], 2]
+%              [[101, 1], [104, 1]]
+             
+             
+
